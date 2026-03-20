68d8037e08e2d2060413508a20bb7c0343ef59e8
mahammad-eldarov <mehemmedeldarov@gmail.com>
2026-03-20 15:25:12 +0400

    init commit

--- src/main/java/az/bank/paymentsystem/util/card/CardOrderRejectionHandler.java
+++ src/main/java/az/bank/paymentsystem/util/card/CardOrderRejectionHandler.java
@@ -7,6 +7,7 @@ import az.bank.paymentsystem.enums.OrderType;
 import az.bank.paymentsystem.exception.ExceptionResponse;
 import az.bank.paymentsystem.exception.MultiValidationException;
 import az.bank.paymentsystem.repository.CardOrderRepository;
+import az.bank.paymentsystem.service.NotificationService;
 import az.bank.paymentsystem.service.OrderRateLimitService;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
@@ -18,17 +19,34 @@ public class CardOrderRejectionHandler {
 
     private final OrderRateLimitService orderRateLimitService;
     private final CardOrderRepository cardOrderRepository;
+    private final NotificationService notificationService;
 
     public void handleRejection(CardOrderEntity orderEntity,
                                 CustomerEntity customer,
                                 MultiValidationException ex) {
         orderEntity.setStatus(OrderStatus.REJECTED);
-        orderEntity.setRejectionReason(
-                ex.getErrors().stream()
+        String reason = ex.getErrors().stream()
                         .map(ExceptionResponse::getMessage)
-                        .collect(Collectors.joining(", "))
-        );
+                        .collect(Collectors.joining(", "));
+        orderEntity.setRejectionReason(reason);
         orderRateLimitService.handleRejection(customer, OrderType.CARD);
         cardOrderRepository.save(orderEntity);
+        notificationService.send(customer,
+                "Your card order request has been rejected. Reason: " + reason);
     }
+
+//    public void handleRejection(CardOrderEntity orderEntity,
+//                                CustomerEntity customer,
+//                                MultiValidationException ex) {
+//        orderEntity.setStatus(OrderStatus.REJECTED);
+//        orderEntity.setRejectionReason(
+//                ex.getErrors().stream()
+//                        .map(ExceptionResponse::getMessage)
+//                        .collect(Collectors.joining(", "))
+//        );
+//        orderRateLimitService.handleRejection(customer, OrderType.CARD);
+//        cardOrderRepository.save(orderEntity);
+//        notificationService.send(customer,
+//                "Your card order request has been rejected. Reason: " + reason);
+//    }
 }
